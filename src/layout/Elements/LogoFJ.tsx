400f59af218e2965ba1427c2db4f1be824b3eaae
toshinori8 <zbigniew.karski@gmail.com>
2022-03-10 11:21:59 +0100

    logo SVG repair

--- src/layout/Elements/LogoFJ.tsx
+++ src/layout/Elements/LogoFJ.tsx
@@ -1,33 +1,39 @@
 import Link from "next/link"
-import { constructor } from "react"
+
 
 function LogoFJ() {
   return (
     <Link href="/">
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 298 118.7">
+
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 298 118.7">
+    <style>
+  </style>
     <defs>
         <mask id="Mask-1">
-            <g transform="translate(-14.3,42.7)">
-                <path d="M0 0c0 0 8.5 10.7 8.5 10.7c0 0-0.8 4.6-2 6.3c-1.3 1.7-4.7 5.2-6.5 6.7c-1.8 1.5 3.7-13 3.7-13l-9-10.7l-3.2-5l3.2-4" fill="#fff" transform="translate(89.8,6.3)" className="animLogoMask1d" />
-                <path d="M0 0l13-6.1l7-4.9l8-6.2l17-4.8l4-8.2l-60 30.2" fill="#fff" transform="translate(-12.7,106.3)"  className="animLogoMask1c" />
-                <path d="M0 0l-12.4 5l-11.3 5l-13 3" fill="#fff" transform="translate(92.6,-51.7)" className="animLogoMask1b" />
-                <path d="M0 0l3.4-10.3l49 10.3l-5.7 16.9l-17.3-6.9" fill="#fff" transform="translate(143.9,86.3)" className="animLogoMask1a"/>
+            <g transform="translate(75.7,-13.1)">
+                <rect width="8.1" height="21" fill="#fff" transform="translate(42.4,62.9) rotate(60) translate(-66.6,-10.5)" className="animMask1"/>
+                <rect width="8.1" height="21" fill="#fff" transform="translate(42.4,62.9) rotate(60) translate(-66.6,-10.5)" className="animMask2"/>
+                <rect width="3.3" height="17.8" fill="#fff" transform="translate(138.1,122.8) skewX(33) translate(-46,-14)" className="animMask3"/>
+                <rect width="8.1" height="21" fill="#fff" transform="translate(17.4,69.3) rotate(60) translate(78.9,-10.5)" className="animMask4"/>
+                <rect width="19.5" height="3" fill="#fff" transform="translate(56.5,101.2) rotate(31) scale(-1,-1) translate(81,44.4)" className="animMask5"/>
+                <rect width="19.5" height="3" fill="#fff" transform="translate(56.5,101.2) rotate(31) scale(-1,-1) translate(55.6,-100.6)" className="animMask6"/>
             </g>
         </mask>
         <mask id="Mask-2">
-            <path d="M0 0l1 7h-38l-3-9l3-3.3l7.9 3.3" fill="#a51a42" transform="translate(123,124)" className="animLogoMask2"/>
+            <rect width="22.6" height="9.9" fill="#fff" transform="translate(132.3,88.2) rotate(-149) translate(33,-80.5)" className="animMask7"/>
         </mask>
         <mask id="Mask-3">
-            <rect width="11.9" height="135" fill="#fff" transform="translate(99.8,59.5) translate(-6,-67.5)" className="animLogoMask3" />
+            <rect width="11.9" height="135" fill="#fff" transform="translate(99.8,59.5) translate(-6,-67.5)" className="animMask8"/>
+        </mask>
+        <mask id="Mask-4">
+            <rect width="14.6" height="111.1" fill="#fff" transform="translate(163.6,8.6)" className="animMask9"/>
         </mask>
     </defs>
-    <style>
- </style>
     <path className="st1" d="M71.2 1l-71.2 117.7h46.2l32.1-53.7l-9.6-15.1l-32.1 51.3h-7l40.9-67.2l49.5 84.7h49.5l-72.7-118.7l-9.3 15.6l49.8 85.3h-7.7Z" fill="#a51a42" mask="url(#Mask-1)"/>
     <path className="st1" d="M93.9 118.7h17.3l-28.2-45.9l-28.4 45.9h16.6l10.8-17.4Z" fill="#a51a42" mask="url(#Mask-2)"/>
     <path className="st1" d="M126 0h-14.6h-5.6l72.8 119.9h7.6h12.6h99.2v-119.9h-172Z" fill="#a51a42" mask="url(#Mask-3)"/>
+    <path className="st1" d="M185.6 108.3h21v-39h42.8v-17h-43.3v-23h46.2v-17.2h-66.7Zm98.7-16.3c1.5-4.5 1.7-13.3 1.7-13.3h-0.2v-66.6h-20l-0.5 55.9c0 0 .8 12.2-1.1 17.3c-1.9 5.1-5.5 5.6-7.9 6c-2.4 .4-7-0.1-7-0.1l-4.2-0.4l-0.2 16.6c0 0 .6 .2 2.4 .6c1.8 .4 10.8 1 15.2 .8c4.4-0.2 9.7-1.9 13.7-4.4c4-2.5 6.7-7.9 8.1-12.4Z" fill="#fff" fill-rule="evenodd" mask="url(#Mask-4)" transform="translate(.4,-1)"/>
 </svg>
-
 
       </Link>
   )
