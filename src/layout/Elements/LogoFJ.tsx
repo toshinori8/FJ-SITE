69ba63fa50e8c1d2ffb9a295329e0697bb878a93
toshinori8 <zbigniew.karski@gmail.com>
2022-03-10 08:15:28 +0100

    updates logo

--- src/layout/Elements/LogoFJ.tsx
+++ src/layout/Elements/LogoFJ.tsx
@@ -1,155 +1,33 @@
 import Link from "next/link"
+import { constructor } from "react"
 
 function LogoFJ() {
   return (
     <Link href="/">
-    <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 298 118.7"
-    >
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 298 118.7">
     <defs>
-      <mask id="adps4jihkkv3v656wmccfulc">
-        <g
-              transform="translate(34.8571,92.0349) translate(-49.1429,-49.3437)"
-            >
-              <path
-                d="M0,0C0,0,8.47428,10.7,8.47428,10.7C8.47428,10.7,7.7,15.3,6.45,17C5.2,18.7,1.8,22.2,0,23.7C-1.8,25.2,3.7,10.7,3.7,10.7L-5.3,0L-8.52572,-4.99116L-5.3,-8.99116"
-                fill="#ffffff"
-                strokeLinecap="square"
-                stroke="none"
-                transform="translate(89.8114,6.3)"
-  
-                style={{  animation: '4.1s linear both o7gvxdxspslztuvgswjplgkg_f, 4.1s linear both o7gvxdxspslztuvgswjplgkg_d'
-                }}
-  
-             
-              />
-              <path
-                d="M0,0L13,-6.09996L20,-11L28,-17.2L45,-21.95L49,-30.25L-11,0"
-                fill="#ffffff"
-                strokeLinecap="square"
-                stroke="none"
-                transform="translate(-12.7143,106.259)"
-                style={{animation: '4.1s linear both aynzm8tnaa70b3wkozxaf4vy_t, 4.1s linear both aynzm8tnaa70b3wkozxaf4vy_d'}}
-              />
-              <path
-                d="M0,0L-12.3743,5L-23.7,10L-36.7,13"
-                fill="#ffffff"
-                strokeLinecap="square"
-                stroke="none"
-                transform="translate(92.5857,-51.6912)"
-                style={{animation: '4.1s linear both q1oich5bfl92ufh2ubv4pljt_d'}}
-              />
-              <path
-                d="M0,0L3.4,-10.3L52.4,0L46.7,16.95L29.4,10"
-                fill="#ffffff"
-                strokeLinecap="square"
-                stroke="none"
-                transform="translate(143.886,86.3088)"
-                 style={{animation: '4.1s linear both himrf3zc2zdm4133uliknk0j_d'}}
-              />
+        <mask id="Mask-1">
+            <g transform="translate(-14.3,42.7)">
+                <path d="M0 0c0 0 8.5 10.7 8.5 10.7c0 0-0.8 4.6-2 6.3c-1.3 1.7-4.7 5.2-6.5 6.7c-1.8 1.5 3.7-13 3.7-13l-9-10.7l-3.2-5l3.2-4" fill="#fff" transform="translate(89.8,6.3)" className="animLogoMask1d" />
+                <path d="M0 0l13-6.1l7-4.9l8-6.2l17-4.8l4-8.2l-60 30.2" fill="#fff" transform="translate(-12.7,106.3)"  className="animLogoMask1c" />
+                <path d="M0 0l-12.4 5l-11.3 5l-13 3" fill="#fff" transform="translate(92.6,-51.7)" className="animLogoMask1b" />
+                <path d="M0 0l3.4-10.3l49 10.3l-5.7 16.9l-17.3-6.9" fill="#fff" transform="translate(143.9,86.3)" className="animLogoMask1a"/>
             </g>
-          </mask>
-          <mask id="b4b5fgdv0igm2hqixy3qg93e">
-            <path
-              d="M0,0L1,7L-37,7L-40,-2L-37,-5.3L-29.1,-2"
-              fill="#a51a42"
-              strokeLinecap="square"
-              stroke="none"
-              transform="translate(123,124)"
-              style={{ animation: '4.1s linear both ik8repdqtyeiuhzaj728e9n8_f,4.1s linear both ik8repdqtyeiuhzaj728e9n8_d'}}
-            />
-          </mask>
-          <mask id="d2dg5xyh47xveu20djmnumtu">
-            <rect
-              id="m7af00q3gfzeqnzc93o32n0w"
-              width="11.9"
-              height="135"
-              fill="#ffffff"
-              stroke="none"
-              transform="translate(99.85,59.5) translate(-5.95,-67.5)"
-               style= {{animation: '4.1s linear both m7af00q3gfzeqnzc93o32n0w_t, 4.1s linear both m7af00q3gfzeqnzc93o32n0w_f, 4.1s linear both m7af00q3gfzeqnzc93o32n0w_w'}}
-              
-            />
-          </mask>
-          <mask id="akax0jm6u61gpdgjcl4q0i5e">
-            <path
-              id="xgikma94rngacn3x8d6hk12c"
-              className="st1"
-              d="M185.6,108.3L206.6,108.3L206.6,69.3L249.4,69.3L249.4,52.3L206.1,52.3L206.1,29.3L252.3,29.3L252.3,12.1L185.6,12.1ZM284.3,92C285.8,87.5,286,78.7,286,78.7L285.8,78.7L285.8,12.1L265.8,12.1L265.3,68C265.3,68,266.1,80.2,264.2,85.3C262.3,90.4,258.7,90.9,256.3,91.3C253.9,91.7,249.3,91.2,249.3,91.2L245.1,90.8L244.9,107.4C244.9,107.4,245.5,107.6,247.3,108C249.1,108.4,258.1,109,262.5,108.8C266.9,108.6,272.2,106.9,276.2,104.4C280.2,101.9,282.9,96.5,284.3,92Z"
-              fill="#fbfbfb"
-              fillRule="evenodd"
-              transform="translate(30.3,53.75) translate(-201.9,-59.95)"
-            />
-          </mask>
-        </defs>
-  
-        <path
-          id="z9i7zmeyo7lhcqogri9fyjor"
-          className="st1"
-          d="M71.2,1L0,118.7L46.2,118.7L78.3,65L68.7,49.9L36.6,101.2L29.6,101.2L70.5,34L120,118.7L169.5,118.7L96.8,0L87.5,15.6L137.3,100.9L129.6,100.9Z"
-          fill="#A51A42"
-          mask="url(#adps4jihkkv3v656wmccfulc)"
-          transform="translate(84.75,59.35) translate(-84.75,-59.35)"
-        />
-        <path
-          id="tdqrs5e5t2g9earq46gwz52e"
-          className="st1"
-          d="M93.9,118.7L111.2,118.7L83,72.8L54.6,118.7L71.2,118.7L82,101.3Z"
-          fill="#A51A42"
-          mask="url(#b4b5fgdv0igm2hqixy3qg93e)"
-          transform="translate(82.9,95.75) translate(-82.9,-95.75)"
-        />
-        <path
-          id="hq9vgqjk0c7mb3lystdpl55z"
-          className="st1"
-          d="M126,0L111.4,0L105.8,0L178.6,119.9L186.2,119.9L198.8,119.9L298,119.9L298,0L126,0Z"
-          fill="#A51A42"
-          mask="url(#d2dg5xyh47xveu20djmnumtu)"
-          transform="translate(201.9,59.95) translate(-201.9,-59.95)"
-        />
-        <g
-          opacity="0"
-          mask="url(#akax0jm6u61gpdgjcl4q0i5e)"
-          transform="translate(214.15,56.75) translate(-42.15,-51.55)"
-          style={{animation: '4.1s linear both px70rz0i34hotqrvrrt28pla_o'}}
-        >
-          <path
-            fill="#ffffff"
-            stroke="none"
-            d="M0,0L33,0L33,4.9L0,4.9Z"
-            transform="translate(103.5,2.45) translate(-16.5,-2.45)"
-            style={{animation: '4.1s linear both ctk2b4819y8wzn8u8zfw6d31_f, 4.1s linear both ctk2b4819y8wzn8u8zfw6d31_d'}}
-          />
-          <rect
-            width="24.6"
-            height="4.9"
-            fill="#ffffff"
-            stroke="none"
-            transform="translate(22.3,4.85) translate(-12.3,-2.05)"
-            style={{animation: '4.1s linear both m7jkcovhs7h9o4zgjxj7yv8z_t, 4.1s linear both m7jkcovhs7h9o4zgjxj7yv8z_f,4.1s linear both m7jkcovhs7h9o4zgjxj7yv8z_h'}}
-  
-          />
-          <rect
-            width="6.6"
-            height="21.9"
-            fill="#ffffff"
-            stroke="none"
-            transform="translate(3.3,17.85) translate(-3.3,-10.95)"
-            style= {{ animation: '4.1s linear both gqdo0hiesatwy4467qupey2d_t, 4.1s linear both gqdo0hiesatwy4467qupey2d_f, 4.1s linear both gqdo0hiesatwy4467qupey2d_w'}}
-            
-          />
-          <rect
-            width="4.6"
-            height="25.8"
-            fill="#ffffff"
-            stroke="none"
-            transform="translate(4.3,54.7) translate(-2.3,-12.9)"
-           style={{animation: '4.1s linear both ua3aswkj40d69zhl6dljlnju_f, 4.1s linear both ua3aswkj40d69zhl6dljlnju_w'}}
-           
-          />
-        </g>
-      </svg>
+        </mask>
+        <mask id="Mask-2">
+            <path d="M0 0l1 7h-38l-3-9l3-3.3l7.9 3.3" fill="#a51a42" transform="translate(123,124)" className="animLogoMask2"/>
+        </mask>
+        <mask id="Mask-3">
+            <rect width="11.9" height="135" fill="#fff" transform="translate(99.8,59.5) translate(-6,-67.5)" className="animLogoMask3" />
+        </mask>
+    </defs>
+    <style>
+ </style>
+    <path className="st1" d="M71.2 1l-71.2 117.7h46.2l32.1-53.7l-9.6-15.1l-32.1 51.3h-7l40.9-67.2l49.5 84.7h49.5l-72.7-118.7l-9.3 15.6l49.8 85.3h-7.7Z" fill="#a51a42" mask="url(#Mask-1)"/>
+    <path className="st1" d="M93.9 118.7h17.3l-28.2-45.9l-28.4 45.9h16.6l10.8-17.4Z" fill="#a51a42" mask="url(#Mask-2)"/>
+    <path className="st1" d="M126 0h-14.6h-5.6l72.8 119.9h7.6h12.6h99.2v-119.9h-172Z" fill="#a51a42" mask="url(#Mask-3)"/>
+</svg>
+
 
       </Link>
   )
